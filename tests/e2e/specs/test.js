96e63fc9785e81cce8675fdf79b68198ad630572
Andrew Hertel <hertelandy@gmail.com>
2019-08-02 14:49:19 -0700

    Adding unit tests and e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,93 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('root tests', () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.contains('h1', 'Welcome to Your Vue.js App');
+  });
+
+  /**
+   * Ensures app has loaded properly
+   */
+  it('Visits the app root url', () => {
+    cy.contains('h1', 'Your Amazing ToDo List');
+  });
+
+  /**
+   * Ensures input number of todo items are rendered
+   */
+  it('should have three todo items', () => {
+    cy
+      .get('[class=list-container]')
+      .children()
+      .should('have.length', 3)
+  });
+
+  /**
+   * Test that clicking check mark completes an item
+   */
+  it('should mark complete', () => {
+    cy
+      .get('[class=complete-icon]')
+      .children()
+      .should('have.class', 'fa-square')
+    cy
+      .get('[class=complete-icon]')
+      .click({multiple: true})
+
+    cy
+      .get('[class=complete-icon]')
+      .children()
+      .should('have.class', 'fa-check-square')
+  });
+
+  /**
+   * Test whether 'x' button removes items
+   */
+  it('should remove item', () => {
+    cy
+      .get('[class=list-container]')
+      .children()
+      .should('have.length', 3)
+
+    cy
+      .get('[class=remove-item-button]')
+      .first()
+      .click()
+
+    cy
+      .get('[class=list-container]')
+      .children()
+      .should('have.length', 2)
+  });
+
+  /**
+   * Test sorting functionality based on priority
+   */
+  it('should sort', () => {
+    cy
+      .get('[class=priority-pill]')
+      .children()
+      .first()
+      .contains('important');
+
+    cy
+      .get('[id=sort-button]')
+      .click()
+
+    cy
+      .get('[class=priority-pill]')
+      .children()
+      .first()
+      .contains('life changing');
+
+    cy
+      .get('[id=sort-button]')
+      .click()
+
+    cy
+      .get('[class=priority-pill]')
+      .children()
+      .first()
+      .contains('meh');
   });
 });
--- tests/unit/todo-list.component.spec.js
+++ tests/unit/todo-list.component.spec.js
@@ -1,55 +1,55 @@
-// import { expect } from 'chai';
-// import { shallowMount } from '@vue/test-utils';
-// import HelloWorld from '@/components/HelloWorld.vue';
-
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message';
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg },
-//     });
-//     expect(wrapper.text()).to.include(msg);
-//   });
-// });
-
 import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import TodoListComponent from '@/components/todo-list/todo-list.component.vue';
-import TodoItemComponent from '@/components/todo-item/todo-item.component.vue'
-import StandardButtonComponent from '@/components/standard-button/standard-button.component.vue'
-import NewTodoItemComponent from '@/components/todo-item/new-todo-item/new-todo-item.component.vue'
+import StandardButtonComponent from '@/components/standard-button/standard-button.component.vue';
+import NewTodoItemComponent from '@/components/todo-item/new-todo-item/new-todo-item.component.vue';
 import * as $cookies from "vue-cookies";
+import TodoItemComponent from '@/components/todo-item/todo-item.component.vue';
 
 describe('todo-list.component.vue', () => {
-  it('renders msg', () => {
+    let wrapper;
     const msg = 'Your Amazing ToDo List';
-    const wrapper = shallowMount(TodoListComponent, {
-      propsData: { msg },
-      mocks: {
-        $cookies: {
-          config: () => {},
-          get: () => {},
-          set: () => {}
-        }
-      }
+    before(() => {
+        wrapper = mount(TodoListComponent, {
+            propsData: { msg },
+            mocks: {
+                $cookies: {
+                config: () => {},
+                get: () => {},
+                set: () => {}
+                }
+            }
+        });
     })
-    expect(wrapper.text()).to.include(msg);
-    // const test = true;
-    // expect(test).to.be.true;
-  });
-
-  it('changes sort from ascending to descending', () => {
-    const wrapper = shallowMount(TodoListComponent, {
-      mocks: {
-        $cookies: {
-          config: () => {},
-          get: () => {},
-          set: () => {}
-        }
-      }
+    it('renders msg', () => {
+        expect(wrapper.text()).to.include(msg);
     });
-    expect(wrapper.vm.sortAscending).to.be.true;
-    wrapper.find('StandardButtonComponent').trigger('click');
-    expect(wrapper.vm.sortAscending).to.be.false;
-  });
+    
+    it('has three buttons', () => {
+        expect(wrapper.findAll(StandardButtonComponent).length).to.equal(3);
+    });
+
+    it('shows new todo item creation upon button click', () => {
+        expect(wrapper.findAll(NewTodoItemComponent).length).to.equal(0);
+        wrapper.find("#add-button").trigger('click');
+        expect(wrapper.findAll(NewTodoItemComponent).length).to.equal(1);
+    });
+
+    it('changes sort from ascending to descending', () => {
+        expect(wrapper.vm.sortAscending).to.be.true;
+        wrapper.find("#sort-button").trigger('click');
+        expect(wrapper.vm.sortAscending).to.be.false;
+    });
+});
+
+describe('todo-item.component.vue', () => {
+    it('changes completion from false to true', () => {
+        const todoItem = {complete: false};
+        const wrapper = mount(TodoItemComponent, {
+            propsData: { todoItem },
+        });
+        expect(wrapper.vm.todoItem.complete).to.be.false;
+        wrapper.find(".complete-icon").trigger('click');
+        expect(wrapper.vm.todoItem.complete).to.be.true;
+    });
 });
